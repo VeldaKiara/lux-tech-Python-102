d7aadd4d04ce5dd76bb8c049ddf8ae9f06912271
Velda Kiara <32552296+veldakarimi@users.noreply.github.com>
2022-02-13 18:24:50 +0300

    control flow code

---
+++ control-flow.py
@@ -0,0 +1,36 @@
+#sequential
+a=40
+b=20
+c=a-b
+print("Subtraction is : ",c)
+
+#selection statements
+#simple if statements
+n = 10
+if n % 2 == 0:
+   print("n is an even number")
+    
+#if-else statements
+n = 5
+if n % 2 == 0:
+   print("n is even")
+else:
+   print("n is odd") #5 is odd
+    
+    
+l = 10
+u = 12
+x = 17
+if l > u:
+   if l > x:
+      print("l value is big")
+   else:
+       print("x value is big")
+elif u > x:
+    print("u value is big")
+else:
+     print("x is big")   #x is big
+    
+
+    
+    
